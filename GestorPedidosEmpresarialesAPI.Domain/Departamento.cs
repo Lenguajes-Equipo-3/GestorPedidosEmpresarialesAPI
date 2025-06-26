70b53415af34147f2eb007d729149f9ae5649fb6
Kevin080899 <63740701+Kevin080899@users.noreply.github.com>
2025-06-25 20:20:33 -0600

    Clases Domain
    Clases Domain

---
+++ GestorPedidosEmpresarialesAPI.Domain/Departamento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorPedidosEmpresarialesBackend.Domain
+{
+    public class Departamento
+    {
+        private int idDepartamento;
+        private String nombreDepartamento;
+        private bool eliminado;
+
+        public Departamento(int idDepartamento, string nombreDepartamento, bool eliminado)
+        {
+            this.idDepartamento = idDepartamento;
+            this.nombreDepartamento = nombreDepartamento;
+            this.eliminado = eliminado;
+        }
+
+        public int IdDepartamento { get => idDepartamento; set => idDepartamento = value; }
+        public string NombreDepartamento { get => nombreDepartamento; set => nombreDepartamento = value; }
+        public bool Eliminado { get => eliminado; set => eliminado = value; }
+
+    }
+}
---
+++ GestorPedidosEmpresarialesAPI.Domain/VarianteProducto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorPedidosEmpresarialesBackend.Domain
+{
+    public class VarianteProducto
+    {
+        private int idVariante;
+        private int idProductoBase;
+        private string talla;
+        private string descripcion;
+        private decimal precio;
+        private decimal cantidadExistencias;
+        private int puntoReorden;
+        private bool eliminado;
+
+        public VarianteProducto(int idVariante, int idProductoBase, string talla, string descripcion, decimal precio, decimal cantidadExistencias, int puntoReorden, bool eliminado)
+        {
+            this.idVariante = idVariante;
+            this.idProductoBase = idProductoBase;
+            this.talla = talla;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            this.cantidadExistencias = cantidadExistencias;
+            this.puntoReorden = puntoReorden;
+            this.eliminado = eliminado;
+        }
+
+        public int IdVariante { get => idVariante; set => idVariante = value; }
+        public int IdProductoBase { get => idProductoBase; set => idProductoBase = value; }
+        public string Talla { get => talla; set => talla = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public decimal Precio { get => precio; set => precio = value; }
+        public decimal CantidadExistencias { get => cantidadExistencias; set => cantidadExistencias = value; }
+        public int PuntoReorden { get => puntoReorden; set => puntoReorden = value; }
+        public bool Eliminado { get => eliminado; set => eliminado = value; }
+
+    }
+}
