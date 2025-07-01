5a55c2783e4e2893f6730464a43ce8d4a81b7bc6
Kevin080899 <63740701+Kevin080899@users.noreply.github.com>
2025-07-01 15:10:07 -0600

    Api Categoria
    Categoria

---
+++ GestorPedidosEmpresarialesBackend.Business/CategoriaBusiness.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GestorPedidosEmpresarialesBackend.Data;
+using GestorPedidosEmpresarialesBackend.Domain;
+
+namespace GestorPedidosEmpresarialesBackend.Business
+{
+    public class CategoriaBusiness
+    {
+        private readonly CategoriaData categoriaData;
+
+        public CategoriaBusiness(CategoriaData categoriaData)
+        {
+            this.categoriaData = categoriaData;
+        }
+
+        public List<Categoria> GetAllCategorias() => categoriaData.GetAllCategorias();
+
+        public Categoria? GetCategoriaById(int id) => categoriaData.GetCategoriaById(id);
+
+        public void AddCategoria(Categoria categoria) => categoriaData.InsertCategoria(categoria);
+
+        public void DeleteCategoria(int id) => categoriaData.DeleteCategoria(id);
+    }
+}
